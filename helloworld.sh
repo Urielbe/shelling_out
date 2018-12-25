c128ec388ba72a961fc55df17476463162f4de25
OrBraha <46023692+OrBraha@users.noreply.github.com>
2018-12-25 22:29:41 +0200

    Add files via upload

---
+++ helloworld.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+## Or Braha ##
+## Hello World Script ##
+## Homework - 25.12.2018 ##
+ 
+printf "\n\n" 
+printf "***********************\n" 
+printf "**** Hello World ****** \n"
+printf "*********************** \n "
+printf "\n\n"
